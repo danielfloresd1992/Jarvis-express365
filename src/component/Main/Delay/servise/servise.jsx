8d48baabdbf7b3e5e9d3225dd1f1893e5d15a6a1
Daniel <danielfloresd92@gmail.com>
2025-09-15 20:57:16 -0400

    Correción en componente services

--- src/component/Main/Delay/servise/servise.jsx
+++ src/component/Main/Delay/servise/servise.jsx
@@ -1,8 +1,8 @@
-import { useState, useRef, useEffect } from 'react';
-import { isMobile } from 'react-device-detect';
+import { useState, useRef } from 'react';
+
 import { useSelector } from 'react-redux';
 import { useAlert } from '../../../../hook/useAlert';
-import { Search } from '../../search/searchComponent.jsx';
+
 import { useSaveNoveltie } from '../../../../hook/useSaveNoveltie';
 import { useDataUser } from '../../../../hook/useTextMenu.jsx';
 import { ImgBoxImg } from '../../imgBoxComponent/ImgBox';
@@ -12,39 +12,38 @@ import URL from '../../../../libs/fetch_data/api_conexion.js';
 import useAdapterResize from '../../../../hook/adapter_resize.jsx';
 import { sendFile } from '../../../../libs/fetch_data/multimedia.Fetching.js';
 import { blobToFile } from '../../../../libs/script/64toFile.js';
+import DishInputSelet from '../../../keysInputs/dishInput.jsx';
+
+import { returnTimeExceding } from '../../../../libs/date_time/time.js';
+import { TableInput } from '../../../keysInputs/tableNumber.jsx';
+
 
 
 
 function Servises({ awaitWindow, boxModal, reset, title }) {
 
     const users = useSelector(state => state.users);
-    const locals = useSelector(state => state.locals);
-    const sectionHtmlRef = useRef(null);
+    const seletedEstableshment = useSelector(state => state.establishment);
+
 
     const alert = useAlert();
     const saveNoveltie = useSaveNoveltie();
     let [files, setFiles] = useState([null, null]);
-    const [tableNeeded, setTableNeeded] = useState(true);
+
+
     let [table, setNumberTable] = useState(null);
-    let [dish, setDish] = useState('');
+    let [dish, setDish] = useState(null);
     let [time1, setTime1] = useState('');
-    let [time3, setTime3] = useState('');
-    let [timeTotal, setTimeTotal] = useState('');
+    let [time2, setTime2] = useState('');
     let [description, setDescription] = useState('');
+
     const user = useRef(null);
     let [local, setLocal] = useState(null);
     const { htmlAdapterRef } = useAdapterResize({ breackWidth: 1350 });
 
 
+    const timeTotal = returnTimeExceding(time1, time2);
 
-    useEffect(() => {
-        if (isMobile) {
-
-        }
-        else {
-            setLocal(local = JSON.parse(localStorage.getItem('local_appExpress'))[0]);
-        }
-    }, []);
 
 
 
@@ -69,10 +68,6 @@ function Servises({ awaitWindow, boxModal, reset, title }) {
     };
 
 
-    const fillLocal = id => {
-        const localFranchise = locals.filter(item => id === item._id);
-        setLocal(local = localFranchise[0]);
-    };
 
 
 
@@ -82,14 +77,14 @@ function Servises({ awaitWindow, boxModal, reset, title }) {
             awaitWindow.open('Enviando novedad');
 
             if (local.franchise === 'Mister01') {
-                if (dish === '') return boxModal.open({ title: 'Error', description: 'Seleccione el tipo de plato' });
+                if (!dish) return boxModal.open({ title: 'Error', description: 'Seleccione el tipo de plato' });
             }
 
             let text;
             let descriptionMenu;
             const caption = [];
 
-            const data = useDataUser(user.current, local, sessionStorage.getItem('session'), localStorage.getItem('local_appExpress'));
+            const data = useDataUser(user.current, seletedEstableshment, sessionStorage.getItem('session'), localStorage.getItem('local_appExpress'));
 
             if (data.LANG === 'es') {
                 text = `*${data.localData.name}*\n_*Demora de ${dish !== '' ? dish.toLowerCase() : 'servicio'}*_${table ? `\nMesa: ${table}` : ''}\n${local.alertLength === 'extended' ? `Toma de orden: ${time1}\nEntrega de servicio: ${time3}\nDemora total en servicio: ${timeTotal}` : `Demora en servicio: ${timeTotal}`}${description !== '' ? `\nNota: ${description.toLowerCase()}` : ''}`;
@@ -140,7 +135,7 @@ function Servises({ awaitWindow, boxModal, reset, title }) {
 
             dataForRequest.timePeriod = {
                 init: time1,
-                end: time3,
+                end: time2,
             };
 
             const response = await axiosInstance.post(`${URL}/novelties`, dataForRequest)
@@ -150,10 +145,9 @@ function Servises({ awaitWindow, boxModal, reset, title }) {
                 alert.request(`Novedad en ${data.localData.name}. por validar`);
                 setNumberTable(table = '');
                 setTime1(time1 = '');
-                setTime3(time3 = '');
+                setTime2(time2 = '');
                 setFiles(files = []);
                 setDish(dish = '');
-                setTimeTotal(timeTotal = '');
                 user.current = null;
                 setLocal(local = null);
                 boxModal.open({ title: 'Aviso', description: 'Novedad enviada' });
@@ -173,59 +167,17 @@ function Servises({ awaitWindow, boxModal, reset, title }) {
     };
 
 
-    const printDish = () => {
-        return (
-            <>
-                <label className='box-label' htmlFor=""> Tipo de plato
-                    <select className='box-inputText' onChange={e => { setDish(dish = e.target.value) }} required>
-                        <option value=''>-Seleccione-</option>
-                        <option value={local.lang === 'en' ? 'service' : 'servicio'}>{local.lang === 'en' ? 'service' : 'servicio'}</option>
-                        <option value={local.dishMenu.appetizer}>{local.dishMenu.appetizer}</option>
-                        <option value={local.dishMenu.mainDish}>{local.dishMenu.mainDish}</option>
-                        <option value={local.dishMenu.dessert}>{local.dishMenu.dessert}</option>
-                    </select>
-                </label>
-            </>
-        );
-    };
 
 
 
-    const recepHour = (element) => {
 
-        if (element.id === 'toma-orden') setTime1(time1 = element.value);
-        if (element.id === 'entrega plato') setTime3(time3 = element.value);
-
-        let hourTotal;
-        let minuteTotal;
-        let secondTotal;
-
-        hourTotal = time3.split(':')[0] - time1.split(':')[0];
-        minuteTotal = time3.split(':')[1] - time1.split(':')[1];
-        secondTotal = time3.split(':')[2] - time1.split(':')[2];
-
-        if (secondTotal < 0) {
-
-            secondTotal = 60 - Math.abs(secondTotal)
-            --minuteTotal;
-        }
-        if (minuteTotal < 0) {
-            minuteTotal = 60 - Math.abs(minuteTotal);
-            --hourTotal
-        }
-        if (minuteTotal < 10) minuteTotal = `0${minuteTotal}`
-        if (secondTotal < 10) secondTotal = `0${secondTotal}`
-        if (hourTotal < 9) hourTotal = `0${hourTotal}`;
-
-
-        setTimeTotal(timeTotal = `${isNaN(hourTotal) ? '❌' : hourTotal}:${isNaN(minuteTotal) ? '❌' : minuteTotal}:${isNaN(secondTotal) ? '❌' : secondTotal}`);
-    };
+    return (
+        <>
+            <form className='box-send' onSubmit={e => sendImg(e)}>
+                <h2 style={{ color: 'rgb(92 92 92)', textDecoration: 'underline', textAlign: 'center' }}>{title.es}</h2>
 
 
 
-    function returnForm(localData) {
-        return (
-            <>
                 <div className='box-imgComponenContent' ref={htmlAdapterRef} style={{ zoom: ((window.innerWidth / 1350) - 0.1).toString() }}>
                     {
                         title.photos.caption.map((iteration, index) => (
@@ -236,56 +188,39 @@ function Servises({ awaitWindow, boxModal, reset, title }) {
 
                     }
                 </div>
-                {/*/   setNumberTable   */}
+
                 <div className='box-div-imputContain'>
                     <div className='box-inputContain box-static'>
-                        <label htmlFor="" style={{ display: 'flex', alignItems: 'center', gap: '.5rem', flexDirection: 'column' }}>
-                            <p>¿no se necesita numero de mesa?</p>
-                            <input type='checkbox'
-                                checked={tableNeeded}
-                                onChange={e => {
-                                    console.log(e.target.checked)
-                                    setTableNeeded(e.target.checked)
-                                }}
-                            />
-                        </label>
 
-                        {
-                            tableNeeded ?
-                                <label className='box-label' style={{ color: '#fff' }} > Número de mesa
-                                    <input
-                                        className='box-inputText'
-                                        type="text"
-                                        id="inicio"
-                                        value={table}
-                                        required
-                                        onChange={e => setNumberTable(table = e.target.value)}
-                                    />
-                                </label>
-                                :
-                                null
-                        }
+
+                        <TableInput
+                            value={table}
+                            onChangeEvent={(value) => setNumberTable(value)}
+                        />
 
 
 
 
-                        {
-                            printDish()
-                        }
+                        <DishInputSelet
+                            value={dish}
+                            onChangeEvent={(dish) => setDish(dish)}
+                            dishes={seletedEstableshment?.dishes}
+                        />
+
 
 
 
                         <label className='box-label' htmlFor=""> Toma de orden
                             <input className='box-inputText' type="text" id="toma-orden" value={time1} pattern="^(([0-1]\d)|(2[0-3]))(:[0-5]\d){2}$" required
-                                onChange={e => recepHour(e.target)}
+                                onChange={e => setTime1(e.target)}
                             />
                         </label>
 
 
 
                         <label htmlFor="" className='box-label'> Entrega de servicio
-                            <input className='box-inputText' type="text" id="entrega plato" pattern="^(([0-1]\d)|(2[0-3]))(:[0-5]\d){2}$" value={time3} required
-                                onChange={e => recepHour(e.target)}
+                            <input className='box-inputText' type="text" id="entrega plato" pattern="^(([0-1]\d)|(2[0-3]))(:[0-5]\d){2}$" value={time2} required
+                                onChange={e => setTime2(e.target)}
                             />
                         </label>
 
@@ -296,54 +231,6 @@ function Servises({ awaitWindow, boxModal, reset, title }) {
                         <button className='btnSend' >Enviar</button>
                     </div>
                 </div>
-            </>
-        );
-    }
-
-
-    return (
-        <>
-            <form className='box-send' onSubmit={e => sendImg(e)}>
-                <h2 style={{ color: 'rgb(92 92 92)', textDecoration: 'underline', textAlign: 'center' }}>{title.es}</h2>
-                {
-                    isMobile ?
-                        (
-                            <>
-                                <div className='productionContain-headerContain' style={{ justifyContent: 'center', zIndex: '80' }} >
-                                    <Search array={users} config={{ placeholder: 'Nombre del operador', key: ['name', 'userName'] }} callback={(element, reset) => { return <p onClick={e => { setUser(e.target.id); reset(e.target.textContent) }} className='speed-title' key={element._id} id={element._id} >{`${element.name} ${element.surName}`} </p> }} />
-                                </div>
-                            </>
-                        )
-                        :
-                        (
-                            null
-                        )
-                }
-                {
-                    isMobile ?
-                        (
-                            <>
-                                <div className='productionContain-headerContain' style={{ justifyContent: 'center', zIndex: '40' }} >
-                                    <Search array={locals} config={{ placeholder: 'Nombre del local', key: ['name'] }} callback={(element, reset) => { return <p onClick={e => { fillLocal(e.target.id); reset(e.target.textContent) }} className='speed-title' key={element._id} id={element._id} >{`${element.name}`} </p> }} />
-                                </div>
-                            </>
-                        )
-                        :
-                        (
-                            null
-                        )
-                }
-
-                {
-                    local !== null ?
-                        (
-                            returnForm(local)
-                        )
-                        :
-                        (
-                            null
-                        )
-                }
 
             </form>
         </>
